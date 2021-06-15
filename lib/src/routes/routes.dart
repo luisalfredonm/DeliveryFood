513f07862190881eb8e2008a1cfb9b53de545961
luis <lualfredo@gmail.com>
2021-06-14 22:42:58 -0400

    Welcome Paget

---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,6 @@
+import 'package:deliveryfood/src/pages/welcome_page.dart';
+import 'package:flutter/widgets.dart';
+
+final routes = <String, WidgetBuilder>{
+  'welcome': (BuildContext context) => WelcomePage(),
+};
